d5b97b634222782f07141ad1bb1751896fc55595
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-07-28 11:26:42 -0400

    write password prompt

---
+++ ruby_basics/user_input/password.rb
@@ -0,0 +1,10 @@
+PASSWORD = "purple"
+
+loop do
+  puts ">> Please enter your password:"
+  password_input = gets.chomp
+  break if password_input == PASSWORD
+  puts ">> Invalid password!"
+end
+  
+puts "Welcome!"
