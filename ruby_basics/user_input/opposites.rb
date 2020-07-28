ab53c522b9bfd985d61123012dcd859b1da83df8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-07-28 18:01:26 -0400

    refactor opposites

--- ruby_basics/user_input/opposites.rb
+++ ruby_basics/user_input/opposites.rb
@@ -2,35 +2,27 @@ def valid_number?(number_string)
   number_string.to_i.to_s == number_string && number_string.to_i != 0
 end
 
-loop do
-  puts ">> Please enter a positive or negative integer:"
-  num_string_1 = gets.chomp
-
-  unless valid_number?(num_string_1)
-    next puts "Invalid input. Only non-zero integers are allowed."
-  end
-
-  num_string_2 = nil
+def read_number()
   loop do
     puts ">> Please enter another positive or negative integer"
-    num_string_2 = gets.chomp
-
-    unless valid_number?(num_string_2)
-      next puts "Invalid input. Only non-zero integers are allowed."
-    end
-
-    break
+    num_string = gets.chomp
+    return num_string.to_i if valid_number?(num_string)
+    puts "Invalid input. Only non-zero integers are allowed."
   end
-
-  num1 = num_string_1.to_i
-  num2 = num_string_2.to_i
-
-  if (num1 < 0 && num2 < 0) || (num1 > 0 && num2 > 0)
-    puts ">> Sorry. One integer must be positive, one must be negative."
-    puts ">> Please start over."
-    next
-  end
-
-  sum = num1 + num2
-  break puts "#{num1} + #{num2} = #{sum}"
 end
+
+first_number = nil
+second_number = nil
+
+loop do
+  first_number = read_number
+  second_number = read_number
+
+  break if first_number * second_number < 0
+  
+  puts ">> Sorry. One integer must be positive, one must be negative."
+  puts ">> Please start over."
+end
+
+sum = first_number + second_number
+puts "#{first_number} + #{second_number} = #{sum}"
