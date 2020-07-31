5dc735309c0674a25ff6122f0269ff80f06dc584
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-07-31 17:35:37 -0400

    refactor into ternary operator

--- ruby_basics/conditionals/sun.rb
+++ ruby_basics/conditionals/sun.rb
@@ -1,5 +1,3 @@
 sun = ['visible', 'hidden'].sample
 
-puts "The sun is so bright!" if sun == 'visible'
-
-puts "The clouds are blocking the sun!" unless sun == 'visible'
+puts sun == 'visible' ? "The sun is so bright!" : "The clouds are blocking the sun!"
