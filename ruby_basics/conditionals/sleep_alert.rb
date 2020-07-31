0afa05cefbd3034e44cdf54157ea06c3ac28beae
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-07-31 17:56:53 -0400

    change var name

--- ruby_basics/conditionals/sleep_alert.rb
+++ ruby_basics/conditionals/sleep_alert.rb
@@ -1,5 +1,5 @@
 status = ['awake', 'tired'].sample
 
-duty = if status == 'awake' then "Be productive" else "Go to sleep!" end
+alert = if status == 'awake' then "Be productive" else "Go to sleep!" end
 
-puts duty
+puts alert
