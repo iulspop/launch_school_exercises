8bd447339169e22d83110c8096bbe6dc265c159c
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-08-03 17:53:26 -0400

    select k/v pairs from hash

--- ruby_basics/hashes/exercise_2.rb
+++ ruby_basics/hashes/exercise_2.rb
@@ -9,3 +9,7 @@ numbers.each { |k, v| puts "#{k}: #{v}" }
 half_numbers = numbers.map { |key, number| number / 2 }
 
 p half_numbers
+
+low_numbers = numbers.select { |key, number| number < 25 }
+
+p low_numbers
