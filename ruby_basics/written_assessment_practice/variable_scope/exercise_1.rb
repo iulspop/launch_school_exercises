363ff0fdb24ec7d21d450c49fceea0f6e48fcdd8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-08-28 13:09:47 -0400

    rewrite written assessment answer

--- ruby_basics/written_assessment_practice/variable_scope/exercise_1.rb
+++ ruby_basics/written_assessment_practice/variable_scope/exercise_1.rb
@@ -11,5 +11,15 @@
 
 =begin
 ANSWER
-Calling puts on line 8 will print `7` because integer objects are immutable, so the function call on line 7 doesn't
-affect the object `a` references.
+Calling puts on line 8 will print `7` because integers in Ruby are immutable, so the function call on line 7 cannot
+affect the object `a` references. Thus, `a` is still equal to 7 at the time of the `puts` invocation.
+
+=== ANSWER REWRITE ===
+Reassignment, including assignment operators like `+=` do not mutate objects.
+Instead, it binds the variable to a new object.
+That is, `+=` sets the varaible to point to a new object.
+Since line 4 binds `b` to an new object, the object pointed to by `a` remains unchanged.
+Thus, `a` is stil equal to 7 at the time of the `puts` call.
+ 
+
+=end
