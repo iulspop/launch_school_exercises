e65392e314150e89a46dc1067ce148e967135410
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-10 12:47:57 -0400

    refactor count_occurences

--- ruby_small_problems/easy_1/count_occurrences.rb
+++ ruby_small_problems/easy_1/count_occurrences.rb
@@ -1,19 +1,7 @@
-=begin
-1. initialize empty hash
-2. loop through each array element
-   1. check if hash has a key equal to element
-   2. if true, increment value
-   3. else, create key, set value to 1
-3. loop through hash and pretty print
-=end
-
 def count_occurrences(array)
   occurrences = {}
-  array.each do |el|
-    if occurrences.key?(el) then occurrences[el] += 1
-    else occurrences[el] = 1 end
-  end
-  occurrences.each { |key, value| puts "#{key} => #{value}" }
+  array.uniq.each { |el| occurrences[el] = array.count(el) }
+  occurrences.each { |element, count| puts "#{element} => #{count}" }
 end
 
 vehicles = [
