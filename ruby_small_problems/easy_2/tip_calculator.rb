8a1fb7afe4ddfad0234dbaf2a24783e3fdb03fb5
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-09-11 09:03:42 -0400

    refactor tip_calculator names

--- ruby_small_problems/easy_2/tip_calculator.rb
+++ ruby_small_problems/easy_2/tip_calculator.rb
@@ -2,9 +2,9 @@ puts ">> What is the bill?"
 bill = gets.chomp.to_f
 
 puts ">> What is the tip percentage?"
-tip_percentage = gets.chomp.to_f
+percentage = gets.chomp.to_f
 
-tip = (bill * ( tip_percentage / 100 )).round(2)
+tip = (bill * ( percentage / 100 )).round(2)
 total = (bill + tip).round(2)
 
 puts "The tip is $#{tip}"
