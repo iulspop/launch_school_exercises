e7b3f030c2516a7cdf96b9177ece39bfc80de565
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-10-20 17:48:01 -0400

    refactor string_to_signed_integer

--- ruby_small_problems/easy_4/string_to_signed_integer.rb
+++ ruby_small_problems/easy_4/string_to_signed_integer.rb
@@ -1,13 +1,15 @@
+def string_to_integer(string)
+  string.chars.map { |char| char.ord - 48 }
+  .reduce(0) { |number, digit| number * 10 + digit }
+end
+
 def string_to_signed_integer(string)
   is_negative = string[0].match? '-'
-
-  integer = string.gsub(/[-+]/, '').chars
-  .map { |char| char.ord - 48 }
-  .reduce(0) { |number, digit| number * 10 + digit }
-
+  integer = string_to_integer(string.gsub(/[-+]/, ''))
   is_negative ? -integer : integer
 end
 
 p string_to_signed_integer('4321') == 4321
 p string_to_signed_integer('-570') == -570
 p string_to_signed_integer('+100') == 100
+
