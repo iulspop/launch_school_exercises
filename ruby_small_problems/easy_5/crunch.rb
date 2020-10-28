7e82a1e485f1ba16940c13db7889336797b1e2eb
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-10-28 10:11:35 -0400

    solve crunch

---
+++ ruby_small_problems/easy_5/crunch.rb
@@ -0,0 +1,14 @@
+=begin
+crunch
+
+=end
+
+def crunch(string)
+  string.chars.chunk(&:itself).map(&:first).join
+end
+
+p crunch('ddaaiillyy ddoouubbllee') == 'daily double'
+p crunch('4444abcabccba') == '4abcabcba'
+p crunch('ggggggggggggggg') == 'g'
+p crunch('a') == 'a'
+p crunch('') == ''
