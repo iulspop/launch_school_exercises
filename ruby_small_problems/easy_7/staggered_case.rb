471352b8ab5457c6d4cb4c7393d74565990e1b73
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-11-03 14:08:29 -0500

    solve staggered_case extra

--- ruby_small_problems/easy_7/staggered_case.rb
+++ ruby_small_problems/easy_7/staggered_case.rb
@@ -2,16 +2,18 @@ def staggered_case(string)
   string = string.dup
   alt = true
   string.chars.map do |char|
-    if alt
+    if alt && char.match?(/[a-z]/i)
       alt = !alt
       char.upcase
-    else
+    elsif char.match? /[a-z]/i
       alt = !alt
       char.downcase
+    else
+      char
     end
   end.join
 end
 
-p staggered_case('I Love Launch School!') == 'I LoVe lAuNcH ScHoOl!'
-p staggered_case('ALL_CAPS') == 'AlL_CaPs'
-p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 NuMbErS'
+p staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
+p staggered_case('ALL CAPS') == 'AlL cApS'
+p staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
