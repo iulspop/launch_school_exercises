12fa21cf59cbca829815ef5e8ce6e5e5f4e03407
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-11-06 12:50:51 -0500

    add regex explain

--- ruby_small_problems/medium_2/longest_sentence.rb
+++ ruby_small_problems/medium_2/longest_sentence.rb
@@ -8,10 +8,12 @@ puts "#{largest_sentence}"
 puts "Containing #{number_of_words} words"
 
 =begin
-For further exploration, you can modify their solution slightly to keep the period at the end of each sentence.
+Have you ever wanted to split a text into sentences while including the period in the corect sentence? You can use Regex lookbehind to do that like so:
 You simply modify line 2 to 
-```ruby
-sentences = text.split(/(?<=\.)|(?<=\?)|(?<=!)/)
+```
+sentences = text.split(/(?<=\.)/)
 ```
 This uses the Regex lookbehind `(?<=X)` where X is the pattern. Which asserts: what immediately precedes the current position is the pattern X. It means that it matches a position that meets that assertion. So the regex engine will match not the `\.` or the period character, but the empty string position where the pattern `\.` immediately precedes it. So, the split won't consume the period character, but the empty boundary next to it.
+
+You can read more about it here: http://www.rexegg.com/regex-lookarounds.html
 =end
