9892413b6ba99b0ce1dd7b43206ac3787ce631b3
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-11-08 12:14:50 -0500

    solve tri-angle

---
+++ ruby_small_problems/medium_2/tri-angle.rb
@@ -0,0 +1,13 @@
+def triangle(*angles)
+  return :invalid if angles.sum != 180 || angles.any? { |angle| angle <= 0 }
+
+  return :right  if angles.any? { |angle| angle == 90 }
+  return :obtuse if angles.any? { |angle| angle > 90 }
+  :acute
+end
+
+p triangle(60, 70, 50) == :acute
+p triangle(30, 90, 60) == :right
+p triangle(120, 50, 10) == :obtuse
+p triangle(0, 90, 90) == :invalid
+p triangle(50, 50, 50) == :invalid
