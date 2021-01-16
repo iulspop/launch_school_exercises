2b460ed5b2a20e749747f98ca42c1d13ae892fd6
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-16 07:57:51 -0500

    solve & refactor divisors

---
+++ exercises/RB130/easy_1/divisors.rb
@@ -0,0 +1,11 @@
+def divisors(integer)
+  1.upto(integer).select do |candidate|
+    integer % candidate == 0
+  end
+end
+
+puts divisors(1) == [1]
+puts divisors(7) == [1, 7]
+puts divisors(12) == [1, 2, 3, 4, 6, 12]
+puts divisors(98) == [1, 2, 7, 14, 49, 98]
+puts divisors(99400891) == [1, 9967, 9973, 99400891] # may take a minute
