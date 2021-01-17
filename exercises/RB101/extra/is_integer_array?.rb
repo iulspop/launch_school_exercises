6d245178a609477e8e43046f5c5ff1869cb70ddc
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-17 15:48:03 -0500

    add example solutions

--- exercises/RB101/extra/is_integer_array?.rb
+++ exercises/RB101/extra/is_integer_array?.rb
@@ -12,6 +12,24 @@ end
 
 =end
 
+def is_int_array(arr)
+  return false unless arr.is_a? Array
+  arr.empty? || arr.all? { |num| num.is_a?(Numeric) && num.floor == num }
+end
+
+def is_int_array(arr)
+  arr.is_a?(Array) && arr.all? do
+    |x| x.is_a?(Integer) || x.is_a?(Float) && x.to_i == x
+  end
+end
+
+def is_int_array(arr)
+  return false if arr.class != Array
+  return true if arr.empty?
+  return arr.all? { |n| n.class == Integer || n == n.to_i }
+  false
+end
+
 # test cases
 is_int_array([]) == true
 is_int_array([1, 2, 3, 4]) == true
