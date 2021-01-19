dde2349d9540211db678aa50bbf15eeac0d650fb
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-19 09:01:24 -0500

    add addens solution back

--- exercises/Ruby_Challenges/medium_1/luhn_algorithm.rb
+++ exercises/Ruby_Challenges/medium_1/luhn_algorithm.rb
@@ -5,6 +5,16 @@ class Luhn
 
   def addends
     digits = number.digits.reverse
+    every_second_index_from_right = (digits.length - 2).step(by: -2, to: 0).to_a
+
+    digits.map.with_index do |num, i|
+      if every_second_index_from_right.include? i
+        num = num * 2
+        num > 9 ? (num - 9) : num
+      else
+        num
+      end
+    end
   end
 
   private
