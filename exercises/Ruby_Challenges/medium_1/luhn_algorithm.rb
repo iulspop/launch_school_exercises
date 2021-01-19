f4d9bd12f0862aca73bad74b06a9b2e30aeecdd7
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-19 07:53:51 -0500

    refactor addends for luhn

--- exercises/Ruby_Challenges/medium_1/luhn_algorithm.rb
+++ exercises/Ruby_Challenges/medium_1/luhn_algorithm.rb
@@ -5,9 +5,10 @@ class Luhn
 
   def addends
     digits = number.digits.reverse
-    every_second_index_from_the_right = digits.length.step(by: -2, to: 0).to_a
+    every_second_index_from_right = (digits.length - 2).step(by: -2, to: 0).to_a
+
     digits.map.with_index do |num, i|
-      if every_second_index_from_the_right.include? i
+      if every_second_index_from_right.include? i
         num = num * 2
         num > 9 ? (num - 9) : num
       else
