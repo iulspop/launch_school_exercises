ea8d7e24e0638aaf5c87de6d6bd4d430274efb49
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-20 08:50:22 -0500

    add print test output

--- exercises/RB101/extra/is_integer_array?.rb
+++ exercises/RB101/extra/is_integer_array?.rb
@@ -6,9 +6,9 @@ def is_int_array(arr)
   true
 end
 
-- returns true / True if every element in an array is an integer or a float with no decimals.
-- returns true / True if array is empty.
-- returns false / False for every other input.
+- returns true if every element in an array is an integer or a float with no decimals.
+- returns true if array is empty.
+- returns false for every other input.
 
 =end
 
@@ -31,13 +31,13 @@ def is_int_array(arr)
 end
 
 # test cases
-is_int_array([]) == true
-is_int_array([1, 2, 3, 4]) == true
-is_int_array([-11, -12, -13, -14]) == true
-is_int_array([1.0, 2.0, 3.0]) == true
-is_int_array([1, 2, nil]) == false
-is_int_array(nil) == false
-is_int_array("") == false
-is_int_array([nil]) == false
-is_int_array([1.0, 2.0, 3.0001]) == false
-is_int_array(["-1"]) == false
+p is_int_array([]) == true
+p is_int_array([1, 2, 3, 4]) == true
+p is_int_array([-11, -12, -13, -14]) == true
+p is_int_array([1.0, 2.0, 3.0]) == true
+p is_int_array([1, 2, nil]) == false
+p is_int_array(nil) == false
+p is_int_array("") == false
+p is_int_array([nil]) == false
+p is_int_array([1.0, 2.0, 3.0001]) == false
+p is_int_array(["-1"]) == false
