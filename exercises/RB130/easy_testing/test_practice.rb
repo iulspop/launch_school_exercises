be58ef4ac50db2bdcd829df7181d29fa494611ab
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-21 09:02:28 -0500

    do test practice

--- exercises/RB130/easy_testing/test_practice.rb
+++ exercises/RB130/easy_testing/test_practice.rb
@@ -36,6 +36,39 @@ class Hello < Minitest::Test
   end
 
   def test_list_includes
-    assert_equal ''
+    assert_includes(['yxz'], 'yxz')
+  end
+
+  def test_raise_error
+    employee = Employee.new
+    assert_raises(NoExperienceError) do
+      employee.hire
+    end
+  end
+
+  def test_instance
+    assert_instance_of Numeric, 6
+  end
+
+  def test_kind
+    assert_kind_of Numeric, 6
+  end
+
+  def test_same_object
+    assert_same [0], [0]
+  end
+
+  def test_dd
+    refute_includes [0, 1], 0
+  end
+end
+
+class NoExperienceError < Exception
+
+end
+
+class Employee
+  def hire
+    raise NoExperienceError
   end
 end
