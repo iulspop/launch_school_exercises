cdd1bd7b43024219fe8a4d1a9cf0fac746004d21
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-09-20 10:29:14 -0400

    play with sending requests

---
+++ exercises/JS230/booking_app_node/public/test.js
@@ -0,0 +1,41 @@
+function retrieveSchedules() {
+  const request = new XMLHttpRequest()
+
+  request.open('GET','http://localhost:3000/api/schedules')
+  request.timeout = 5000;
+  request.responseType = 'json'
+
+  request.addEventListener('load', event => {
+    const schedules = request.response
+    const staffs = [];
+    const tally = [];
+
+    if (schedules.length > 0) {
+      schedules.forEach(({staff_id}) => {
+        const key = `staff ${String(staff_id)}`;
+        if (!staffs.includes(key)) {
+          staffs.push(key);
+          tally.push(1);
+        } else {
+          tally[staffs.indexOf(key)] += 1;
+        }
+      });
+
+      alert(tally.map((_, index) => `${staffs[index]}: ${tally[index]}`).join("\n"));
+    } else {
+      alert('There are currently no schedules available for booking');
+    }
+  });
+
+  request.addEventListener('timeout', event => {
+    alert('It is taking longer than usual, please try again later.')
+  });
+
+  request.addEventListener('loadend', event => {
+    alert('The request has completed.');
+  });
+
+  request.send();
+}
+
+retrieveSchedules()
